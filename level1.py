10d296f2c6e93f4029d0c1b6ca9b1301140bbaf1
Kuanishbai Ernar <65388119+Ernar363@users.noreply.github.com>
2020-08-17 22:46:21 +0600

    Update level1.py

--- level1.py
+++ level1.py
@@ -1,12 +1,7 @@
 def level1(N):
 factorial = 1
-if N == 0:
-    return(1)
-elif N == 1:
-    return(1)
-else:
-   for i in range(2, N+1):
-      factorial *= i
-   s = str(factorial)
-   first_digit = int(s[0])
-   return(first_digit)
+for i in range(2, N+1):
+   factorial *= i
+s = str(factorial)
+first_digit = int(s[0])
+print (first_digit)
