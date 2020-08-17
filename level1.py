c9b6c43fabb4c510f1d52acd5cbe61beb86c9356
Kuanishbai Ernar <65388119+Ernar363@users.noreply.github.com>
2020-08-17 22:55:50 +0600

    Update level1.py

--- level1.py
+++ level1.py
@@ -1,7 +1,7 @@
 def level1(N):
-factorial = 1
-for i in range(2, N+1):
-   factorial *= i
-s = str(factorial)
-first_digit = int(s[0])
+   factorial = 1
+   for i in range(2, N+1):
+      factorial *= i
+   s = str(factorial)
+   first_digit = int(s[0])
 return first_digit
